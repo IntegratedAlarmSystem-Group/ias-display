a1052a563b51dad980310e683128f15e767797ef
mxolivares <mx.olivares.p@gmail.com>
2018-01-23 18:03:12 -0500

    Update alarms component with nebular base and related styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,17 @@ import { AppComponent } from './app.component';
 import { AlarmsListComponent } from './alarms-list/alarms-list.component';
 import { AlarmService } from './alarm.service';
 
+import { NbThemeModule } from '@nebular/theme';
+import { NbLayoutModule } from '@nebular/theme';
+import { NbCardModule } from '@nebular/theme';
+// import { NbCardBodyComponent, NbCardFooterComponent, NbCardHeaderComponent}
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path:'', component: AppComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,6 +23,10 @@ import { AlarmService } from './alarm.service';
   ],
   imports: [
     BrowserModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbCardModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AlarmService,
