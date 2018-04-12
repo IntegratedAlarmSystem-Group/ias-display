64393c17c14e5d1b3a45fac93c11a5ad90c05bb3
Sebastian <sebafehlandt@gmail.com>
2018-04-12 15:35:38 -0300

    Change Dockerfile-dev and reunserver-dev.sh in order to use other node_modules directory to avoid conflicts. Closes #22

--- runserver-dev.sh
+++ runserver-dev.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-npm install
-ng serve --host 0.0.0.0 --port 4200 --environment=prod
+chown -R node:node /usr/src/ias-display/node_modules
+su node -c "npm install"
+su node -c "ng serve --host 0.0.0.0 --port 4200 --environment=prod"
