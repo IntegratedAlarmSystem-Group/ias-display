4a60addd5bf0ba64f3bf797ea35a1287e4145ca0
Sebastian <sebafehlandt@gmail.com>
2018-04-16 18:00:15 -0300

    Change Dockerfile to run tests
    - Change karma config in order to use a headless chromium instead of phantomJS
    - Change Dockerfile to run tests

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-phantomjs-launcher'),
-      require('karma-chrome-launcher'),      
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
@@ -28,7 +28,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    customLaunchers: {
+      'ChromeDocker': {
+        base: 'Chrome',
+        flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
+      }
+    },
+    browsers: ['ChromeDocker'],
     singleRun: false
   });
 };
