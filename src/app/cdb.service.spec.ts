2a5de3c2c5c964acbcafa5346637ed191e806703
Maximiliano Olivares <mx.olivares.p@gmail.com>
2018-05-10 15:58:43 -0300

    Add request to get alarm iasios information from the cdb

--- src/app/cdb.service.spec.ts
+++ src/app/cdb.service.spec.ts
@@ -1,4 +1,5 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestRequest } from '@angular/common/http/testing';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
 
@@ -24,7 +25,15 @@ describe('CdbService', () => {
       properties: []
   }];
 
+  let mockIasAlarmsIasiosResponse = [{
+      io_id: "WS-MeteoTB1-Temperature",
+      short_desc: "Temperature reported by the weather station MeteoTB1 out of range",
+      ias_type: "ALARM"
+  }];
+
   let iasCdbUrl = environment.cdbApiUrl+'/ias';
+  let iasioCdbUrl = environment.cdbApiUrl+'/iasio';
+  let iasioCdbAlarmsUrl = iasioCdbUrl+'/filtered_by_alarm';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -45,29 +54,66 @@ describe('CdbService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('should be able to get the ias data configuration from the cdb api at initialization', () => {
+  it('should get the ias data configuration from the cdb and alarms iasios from api at initialization', () => {
     /* Act */
     subject.initialize();
-    const req = testController.expectOne(iasCdbUrl);
-    expect(req.request.method).toEqual('GET');
-    req.flush(mockIasConfigurationResponse);
+    // initialization should trigger two get calls
+    const calls = testController.match(
+        (request) => { return request.method == 'GET' }
+    );
+    expect(calls.length).toEqual(2);
+
+    let iasCall = calls[0];
+    let iasiosCall = calls[1];
+    expect(iasCall.request.url).toEqual(iasCdbUrl);
+    expect(iasiosCall.request.url).toEqual(iasioCdbAlarmsUrl);
+
+    iasCall.flush(mockIasConfigurationResponse);
+    iasiosCall.flush(mockIasAlarmsIasiosResponse);
     testController.verify();
+
     /* Final assert */
     expect(subject.iasConfiguration)
       .toEqual(mockIasConfigurationResponse[0]);
+    expect(subject.iasAlarmsIasios)
+      .toEqual(mockIasAlarmsIasiosResponse);
   });
 
   it('should be able to get the refresh rate after retrieve the configuration data', () => {
-    /* Act */
+    /* Arrange */
     subject.initialize();
-    const req = testController.expectOne(iasCdbUrl);
-    expect(req.request.method).toEqual('GET');
-    req.flush(mockIasConfigurationResponse);
+    const iasCalls = testController.match(
+        (request) => { return request.url == iasCdbUrl });
+    const iasiosCalls = testController.match(
+        (request) => { return request.url == iasioCdbAlarmsUrl });
+    expect(iasCalls.length).toEqual(1);
+    expect(iasiosCalls.length).toEqual(1);
+    iasCalls[0].flush(mockIasConfigurationResponse);
+    iasiosCalls[0].flush(mockIasAlarmsIasiosResponse);
     testController.verify();
-    /* Final assert */
+    /* Act and assert */
     let pars = subject.getRefreshRateParameters();
     expect(pars['refreshRate']).toEqual(2);
     expect(pars['broadcastFactor']).toEqual(3);
+  });
+
+  it('should be able to retrieve the short description for an alarm from the alarm iasio information', () => {
+    /* Arrange */
+    subject.initialize();
+    const iasCalls = testController.match(
+        (request) => { return request.url == iasCdbUrl });
+    const iasiosCalls = testController.match(
+        (request) => { return request.url == iasioCdbAlarmsUrl });
+    expect(iasCalls.length).toEqual(1);
+    expect(iasiosCalls.length).toEqual(1);
+    iasCalls[0].flush(mockIasConfigurationResponse);
+    iasiosCalls[0].flush(mockIasAlarmsIasiosResponse);
+    testController.verify();
+    /* Act and assert */
+    let targetAlarm = mockIasAlarmsIasiosResponse[0]
+    let alarmCoreId = targetAlarm['io_id'];
+    let shortDescription = subject.getAlarmDescription(alarmCoreId);
+    expect(shortDescription).toEqual(targetAlarm['short_desc']);
   });
 
 });
--- src/app/cdb.service.ts
+++ src/app/cdb.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpRequest } from '@angular/common/http';
 
 import { environment } from '../environments/environment'
 
-import {Observable} from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 
 import { HttpClientService } from './http-client.service';
 
@@ -14,12 +14,19 @@ export class CdbService {
   /**
   * Cdb api url from environment settings
   */
-  url = environment.cdbApiUrl+'/ias';
+  iasUrl = environment.cdbApiUrl+'/ias';
+  iasioUrl = environment.cdbApiUrl+'/iasio';
+  iasioAlarmsUrl = this.iasioUrl+'/filtered_by_alarm';
 
   /**
   * Variable to store the ias configuration data
   */
   iasConfiguration;
+  /**
+  * Variable to store alarm type iasios information data
+  */
+  // TODO: Refactor as a dictionary
+  iasAlarmsIasios;
 
   /** Constructor */
   constructor(
@@ -31,17 +38,30 @@ export class CdbService {
   * Method to trigger main request at the beginning
   */
   initialize() {
-    return this.getConfigurationData()
-      .subscribe(
-        data => { this.iasConfiguration = data[0]; }
-    )
+    return Observable.forkJoin(
+      this.getConfigurationData().map((res: Response) => res[0]),
+      this.getAlarmsIasiosData().map((res: Response) => res),
+    ).subscribe(
+      latestValues => {
+        this.iasConfiguration = latestValues[0];
+        this.iasAlarmsIasios = latestValues[1];
+      },
+      err => console.error(err)
+    );
   }
 
   /**
   * Get the ias configuration data from the webserver
   */
   getConfigurationData() {
-    return this.httpClientService.get(this.url);
+    return this.httpClientService.get(this.iasUrl);
+  }
+
+  /**
+  * Get information of alarms iasios from the webserver
+  */
+  getAlarmsIasiosData() {
+    return this.httpClientService.get(this.iasioAlarmsUrl);
   }
 
   /**
@@ -54,6 +74,17 @@ export class CdbService {
       'refreshRate': this.iasConfiguration['refresh_rate'],
       'broadcastFactor': this.iasConfiguration['broadcast_factor']
     }
+  }
+
+  /**
+  * Get short description from iasios information for a selected alarm id
+  *
+  * @param {string} alarmCoreID Alarm identifier for the alarm in the core system
+  */
+  getAlarmDescription(alarmCoreId): string {
+      let targetIasioAlarm = this.iasAlarmsIasios.find(
+        iasio => iasio.io_id === alarmCoreId);
+      return targetIasioAlarm.short_desc;
   }
 
 }
