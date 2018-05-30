9deae2b93f269b83b9912470f078fb1ca48927d7
Sebastian <sebafehlandt@gmail.com>
2018-05-30 15:33:06 -0400

    Change Elements by Alarms

--- src/app/tabular-view/tabular-view.component.ts
+++ src/app/tabular-view/tabular-view.component.ts
@@ -3,10 +3,12 @@ import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { MatTableDataSource } from '@angular/material';
 import { CollectionViewer, DataSource } from "@angular/cdk/collections";
+import { Alarm, OperationalMode, Validity } from '../alarm';
 
 @Injectable()
 export class ElementService {
-  findElements(courseId:number, filter = '', sortOrder = 'asc', pageNumber = 0, pageSize = 3):  Observable<Element[]> {
+  findElements(courseId:number, filter = '', sortOrder = 'asc', pageNumber = 0, pageSize = 3):  Observable<Alarm[]> {
+
     return Observable.of(ELEMENT_DATA);
   }
 }
@@ -18,7 +20,7 @@ export class ElementService {
 })
 export class TabularViewComponent {
 
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns = ['core_id', 'value', 'validity', 'mode'];
   dataSource: ElementsDataSource;
 
   constructor(private elementService: ElementService) {}
@@ -35,15 +37,15 @@ export class TabularViewComponent {
   // }
 }
 
-export class ElementsDataSource implements DataSource<Element> {
+export class ElementsDataSource implements DataSource<Alarm> {
 
-  private elementsSubject = new BehaviorSubject<Element[]>([]);
+  private elementsSubject = new BehaviorSubject<Alarm[]>([]);
   private loadingSubject = new BehaviorSubject<boolean>(false);
   public loading$ = this.loadingSubject.asObservable();
 
   constructor(private elementService: ElementService) {}
 
-  connect(collectionViewer: CollectionViewer): Observable<Element[]> {
+  connect(collectionViewer: CollectionViewer): Observable<Alarm[]> {
       return this.elementsSubject.asObservable();
   }
 
@@ -67,32 +69,53 @@ export class ElementsDataSource implements DataSource<Element> {
 }
 
 
-export interface Element {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+const ELEMENT_DATA: Alarm[] = [
+  Alarm.asAlarm({
+    'value': 0,
+    'core_id': 'coreid$1',
+    'running_id': 'coreid$1',
+    'mode': 0,
+    'core_timestamp': 10000,
+    'validity': 1,
+    'ack': false,
+    'dependencies': [],
+  }),
+  Alarm.asAlarm({
+    'value': 1,
+    'core_id': 'coreid$2',
+    'running_id': 'coreid$2',
+    'mode': 0,
+    'core_timestamp': 10000,
+    'validity': 1,
+    'ack': false,
+    'dependencies': [],
+  }),
+  Alarm.asAlarm({
+    'value': 0,
+    'core_id': 'coreid$3',
+    'running_id': 'coreid$3',
+    'mode': 0,
+    'core_timestamp': 10000,
+    'validity': 1,
+    'ack': false,
+    'dependencies': [],
+  })
+  // {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  // {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  // {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  // {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  // {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  // {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  // {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  // {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  // {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
+  // {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
+  // {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
+  // {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
+  // {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
+  // {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
+  // {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
+  // {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
+  // {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
+  // {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
 ];
