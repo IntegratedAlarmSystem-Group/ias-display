81193b85d7dc21851a4d9eee0557988a1b66ee04
Sebastian <sebafehlandt@gmail.com>
2018-06-04 18:09:20 -0400

    Add OverviewWeatherCardContentComponent tests

--- src/app/overview-weather-card-content/overview-weather-card-content.component.spec.ts
+++ src/app/overview-weather-card-content/overview-weather-card-content.component.spec.ts
@@ -1,14 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, inject, ComponentFixture, TestBed } from '@angular/core/testing';
 import { OverviewWeatherCardContentComponent } from './overview-weather-card-content.component';
+import { RoutingService} from '../routing.service';
 
-xdescribe('OverviewWeatherCardContentComponent', () => {
+
+describe('OverviewWeatherCardContentComponent', () => {
   let component: OverviewWeatherCardContentComponent;
   let fixture: ComponentFixture<OverviewWeatherCardContentComponent>;
+  let routingService: RoutingService;
+  const spyRoutingTable = jasmine.createSpyObj('RoutingService', ['tableWithFilter']);
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OverviewWeatherCardContentComponent ]
+      declarations: [ OverviewWeatherCardContentComponent ],
+      providers: [
+          { provide: RoutingService, useValue: spyRoutingTable }
+      ],
     })
     .compileComponents();
   }));
@@ -21,5 +28,17 @@ xdescribe('OverviewWeatherCardContentComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('WHEN the component calls goToTableFilteredBy', () => {
+    it('THEN the RoutingService.tableWithFilter is called', () => {
+      component.goToTableFilteredBy('mock filter');
+      fixture.whenStable().then(() => {
+        expect(spyRoutingTable.tableWithFilter.calls.count())
+          .toBe(1, 'spy method was called once');
+        expect(spyRoutingTable.tableWithFilter.calls.mostRecent().args[0])
+          .toBe('mock filter', 'spy method was called with the right parameters');
+      });
+    });
   });
 });
