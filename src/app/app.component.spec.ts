f49c46b1cd91e84a3ad1b97281311ea0b645c74b
Jazmine <jamaldonadof@ug.uchile.cl>
2018-06-06 11:59:52 -0400

    Fix some tslinter recommendations

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,25 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { AppComponent } from './app.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
 import { HttpClientService } from './http-client.service';
 import { AlarmService } from './alarm.service';
 import { CdbService } from './cdb.service';
-import { AlarmsListComponent } from './alarms-list/alarms-list.component';
 
 import { NbThemeModule } from '@nebular/theme';
 import { NbLayoutModule } from '@nebular/theme';
 import { NbCardModule } from '@nebular/theme';
 import { NbSidebarModule } from '@nebular/theme';
 import { NbSidebarService } from '@nebular/theme';
-
 import { NbMenuService } from '@nebular/theme';
 import { NbMenuItem } from '@nebular/theme';
 import { NbMenuModule } from '@nebular/theme';
 
-import { RouterModule, Routes } from '@angular/router';
-
+import { AppComponent } from './app.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: 'overview', pathMatch: 'full' },
-  { path:'overview', component: AlarmsListComponent },
-  { path:'weather', component: AlarmsListComponent }
+  { path: '', redirectTo: 'overview', pathMatch: 'full' }
 ];
 
 describe('AppComponent', () => {
@@ -34,8 +30,7 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent,
-        AlarmsListComponent,
+        AppComponent
       ],
       imports: [
         NbThemeModule.forRoot({ name: 'default' }),
