3c5fe1cbb08cfd45cc2a6a250fb3c36d96536fd7
Sebastian <sebafehlandt@gmail.com>
2018-06-26 23:13:43 -0400

    Fix basic Alarm Component test

--- src/app/alarm/alarm.component.spec.ts
+++ src/app/alarm/alarm.component.spec.ts
@@ -1,10 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { AlarmComponent } from './alarm.component';
+import { AlarmComponent, AlarmImageSet } from './alarm.component';
+import { Alarm, Value, OperationalMode } from '../alarm';
 
 describe('AlarmComponent', () => {
   let component: AlarmComponent;
   let fixture: ComponentFixture<AlarmComponent>;
+  const alarm = Alarm.asAlarm({
+    'value': 0,
+    'core_id': 'Dummy-cleared-valid',
+    'running_id': 'Dummy-cleared-valid',
+    'mode': '5',
+    'core_timestamp': 1267252440000,
+    'validity': '1',
+    'ack': false,
+    'dependencies': [],
+  });
+  const location = '/assets/img/';
+  const imageSet = new AlarmImageSet({
+    clear: this.location + 'temp-1.svg',
+    set_low: this.location + 'temp-2.svg',
+    set_medium: this.location + 'temp-2.svg',
+    set_high: this.location + 'temp-3.svg',
+    set_critical: this.location + 'temp-3.svg',
+    unknown: this.location + 'temp-0.svg',
+    maintenance: this.location + 'temp-0.svg',
+  });
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +36,13 @@ describe('AlarmComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AlarmComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    component.alarm = alarm;
+    component.images = imageSet;
+    component.imagesUnreliable = imageSet;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
