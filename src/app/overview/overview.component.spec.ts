3fe911b990950eb37a1dd1cbb4e4c536952ead4f
Sebastian <sebafehlandt@gmail.com>
2018-06-26 23:36:24 -0400

    Fix tests Overview Component

--- src/app/overview/overview.component.spec.ts
+++ src/app/overview/overview.component.spec.ts
@@ -1,10 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { NgbModule, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { IasMaterialModule } from '../ias-material/ias-material.module';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { OverviewComponent } from './overview.component';
 import { OverviewCardComponent } from '../overview-card/overview-card.component';
 import {
   OverviewWeatherCardContentComponent
 } from '../overview-weather-card-content/overview-weather-card-content.component';
 import { RoutingService} from '../routing.service';
+import { HttpClientService } from '../http-client.service';
+import { CdbService } from '../cdb.service';
+import { AlarmService } from '../alarm.service';
+import { AlarmComponent } from '../alarm//alarm.component';
 
 
 describe('OverviewComponent', () => {
@@ -17,11 +25,26 @@ describe('OverviewComponent', () => {
       declarations: [
         OverviewComponent,
         OverviewCardComponent,
-        OverviewWeatherCardContentComponent
+        OverviewWeatherCardContentComponent,
+        AlarmComponent
+      ],
+      imports: [
+        HttpClientModule,
+        NgbModule.forRoot(),
+        IasMaterialModule,
       ],
       providers: [
-          { provide: RoutingService, useValue: spyRoutingTable }
+          { provide: RoutingService, useValue: spyRoutingTable },
+          AlarmService,
+          CdbService,
+          HttpClientService,
+          HttpClient
       ],
+    })
+    .overrideModule( BrowserDynamicTestingModule , {
+      set: {
+        entryComponents: [ AlarmComponent ]
+      }
     })
     .compileComponents();
   }));
