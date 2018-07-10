3ac67961c2e72e1bd3994a4d9315e739e281e7c0
Sebastian <sebafehlandt@gmail.com>
2018-07-10 19:19:05 -0400

    Fix basic ShelveComponent test

--- src/app/actions/shelve/shelve.component.ts
+++ src/app/actions/shelve/shelve.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SidenavService } from '../sidenav.service';
+import { AlarmService } from '../../data/alarm.service';
 
 @Component({
   selector: 'app-shelve',
@@ -12,12 +13,14 @@ export class ShelveComponent implements OnInit, OnDestroy {
   alarm_id: string;
 
   constructor(
+    private alarmService: AlarmService,
     private route: ActivatedRoute,
     public sidenavService: SidenavService
   ) {
   }
 
   ngOnInit() {
+    console.log('this.route', this.route);
     this.route.paramMap.subscribe( paramMap => {
       this.alarm_id = paramMap.get('alarmID');
     });
