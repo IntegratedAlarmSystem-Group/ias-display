06604dbffeafc5ee1f2d74be240943ce825c4f85
Jazmine <jamaldonadof@ug.uchile.cl>
2018-07-10 10:59:04 -0400

    Update sidebar content on url change

--- src/app/shared/shelve/shelve.component.ts
+++ src/app/shared/shelve/shelve.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-shelve',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShelveComponent implements OnInit {
 
-  constructor() { }
+  alarm_id: string;
+
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.route.paramMap.subscribe( paramMap => {
+      this.alarm_id = paramMap.get('alarmID');
+    });
+    console.log('alarm_id = ', this.alarm_id);
   }
 
 }
