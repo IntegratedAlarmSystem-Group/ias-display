644cdc31c9d1af7b61e6b7695de3d9b96138eda8
Jazmine <jamaldonadof@ug.uchile.cl>
2018-07-19 20:37:12 -0400

    Moving alarms logic out of the weather sidebar (WIP)

--- src/app/weather/weather/weather.component.ts
+++ src/app/weather/weather/weather.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSidenav } from '@angular/material';
+import { Observable, BehaviorSubject , SubscriptionLike as ISubscription } from 'rxjs';
+import { AlarmService } from '../../data/alarm.service';
+import { Alarm } from '../../data/alarm';
 
 @Component({
   selector: 'app-weather',
@@ -10,9 +13,74 @@ export class WeatherComponent implements OnInit {
 
   panelOpenState = false;
 
-  constructor() { }
+  /** Alarms Ids grouped by Weather Station**/
+  public WeatherAlarmsIds: {
+    station: string,
+    temperature: string,
+    windspeed: string,
+    humidity: string
+  }[];
+
+  /** Dictionary of Weather Alarms indexed by alarm_id **/
+  public WeatherAlarms: {[core_id: string]: Alarm };
+
+  /** Subscription to changes in the Alarms stored in the {@link AlarmService} */
+  private alarmServiceSubscription: ISubscription;
+
+  /**
+   * Builds an instance of the component
+   * @param {AlarmService} alarmService Service used to get the Alarms
+   */
+  constructor(
+    private alarmService: AlarmService,
+  ) { }
 
   ngOnInit() {
+
+    this.WeatherAlarms['Alarm ID STATION 1'] = Alarm.asAlarm({
+      'value': 1,
+      'core_id': 'Alarm ID STATION 1',
+      'running_id': 'Dummy-cleared-valid',
+      'mode': '5',
+      'core_timestamp': 1267252440000,
+      'validity': '1',
+      'ack': false,
+      'shelved': false,
+      'dependencies': [],
+    });
+    this.WeatherAlarms['Alarm ID TEMP 1'] = Alarm.asAlarm({
+      'value': 1,
+      'core_id': 'Alarm ID TEMP 1',
+      'running_id': 'Dummy-cleared-valid',
+      'mode': '5',
+      'core_timestamp': 1267252440000,
+      'validity': '1',
+      'ack': false,
+      'shelved': false,
+      'dependencies': [],
+    });
+    this.WeatherAlarms['Alarm ID WIND 1'] = Alarm.asAlarm({
+      'value': 1,
+      'core_id': 'Alarm ID WIND 1',
+      'running_id': 'Dummy-cleared-valid',
+      'mode': '5',
+      'core_timestamp': 1267252440000,
+      'validity': '1',
+      'ack': false,
+      'shelved': false,
+      'dependencies': [],
+    });
+    this.WeatherAlarms['Alarm ID HUM 1'] = Alarm.asAlarm({
+      'value': 1,
+      'core_id': 'Alarm ID HUM 1',
+      'running_id': 'Dummy-cleared-valid',
+      'mode': '5',
+      'core_timestamp': 1267252440000,
+      'validity': '1',
+      'ack': false,
+      'shelved': false,
+      'dependencies': [],
+    });
   }
 
 }
