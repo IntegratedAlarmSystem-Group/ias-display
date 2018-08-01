05ce020f09cb17cf255af821f9d61ae12e964dea
Jazmine <jamaldonadof@ug.uchile.cl>
2018-08-01 14:51:32 -0400

    Add a test to check if the getMapData method is returning the map

--- src/app/weather/weather.service.spec.ts
+++ src/app/weather/weather.service.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { DataModule } from '../data/data.module';
 import { WeatherService } from './weather.service';
+import { HttpClientService } from '../data/http-client.service';
 import { Map } from './fixtures';
 import { of } from 'rxjs';
 
+let subject: WeatherService;
+
 describe('WeatherService', () => {
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [WeatherService],
@@ -12,13 +16,21 @@ describe('WeatherService', () => {
     });
   });
 
-  beforeEach(inject([WeatherService], (weatherService) => {
-      spyOn(weatherService, 'getMapData').and.callFake(function() {
-        return of(Map);
-      });
+  beforeEach(inject([WeatherService, HttpClientService], (weatherService, httpClientService) => {
+    subject = weatherService;
+    spyOn(httpClientService, 'get').and.callFake(function() {
+      return of(Map);
+    });
   }));
 
-  it('should be created', inject([WeatherService], (service: WeatherService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created', () => {
+    expect(subject).toBeTruthy();
+  });
+
+  it('should have a method to get the Map', () => {
+    subject.getMapData().subscribe((mapdata) => {
+      expect(mapdata).toEqual(Map);
+    });
+  });
+
 });
