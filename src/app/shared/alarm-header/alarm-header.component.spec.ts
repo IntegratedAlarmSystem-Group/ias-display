748ed93daa00494ef138e51e887e281812159da1
Jazmine <jamaldonadof@ug.uchile.cl>
2018-08-01 15:39:07 -0400

    Add a test to check if the class of the Alarm header is set correctly

--- src/app/shared/alarm-header/alarm-header.component.spec.ts
+++ src/app/shared/alarm-header/alarm-header.component.spec.ts
@@ -1,5 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AlarmHeaderComponent } from './alarm-header.component';
+import { Alarm, Value, OperationalMode } from '../../data/alarm';
+import { MockAlarms } from './fixtures';
+
+const expected_classes = {
+  'critical': ['red'],
+  'high': ['red'],
+  'medium': ['yellow'],
+  'low': ['yellow'],
+  'clear': ['green'],
+  'unknown': ['blue'],
+  'maintenance': ['gray'],
+  'critical_unreliable': ['red', 'unreliable'],
+  'high_unreliable': ['red', 'unreliable'],
+  'medium_unreliable': ['yellow', 'unreliable'],
+  'low_unreliable': ['yellow', 'unreliable'],
+  'clear_unreliable': ['green', 'unreliable'],
+  'unknown_unreliable': ['blue', 'unreliable'],
+  'maintenance_unreliable': ['gray', 'unreliable'],
+};
 
 describe('AlarmHeaderComponent', () => {
   let component: AlarmHeaderComponent;
@@ -15,10 +34,21 @@ describe('AlarmHeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AlarmHeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    component.alarm = Alarm.asAlarm(MockAlarms[0]);
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  for (const alarm of MockAlarms) {
+    it('should display the color of the alarm according to the alarm properties', () => {
+      component.alarm = Alarm.asAlarm(alarm);
+      fixture.detectChanges();
+      expect(component).toBeTruthy();
+      expect(component.getClass()).toEqual(expected_classes[alarm.core_id]);
+    });
+  }
+
 });
