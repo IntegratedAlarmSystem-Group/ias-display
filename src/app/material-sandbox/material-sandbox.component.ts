5fd1633037d1ab360026daf3eb84d72edd4518c1
Max <mx.olivares.p@gmail.com>
2018-08-08 17:00:17 -0400

    Add material sandbox page

---
+++ src/app/material-sandbox/material-sandbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-material-sandbox',
+  templateUrl: './material-sandbox.component.html',
+  styleUrls: ['./material-sandbox.component.css']
+})
+export class MaterialSandboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
