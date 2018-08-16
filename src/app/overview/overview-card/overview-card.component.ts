50b58bde9beb000f9a4dc26619abed5c65499541
Sebastian <sebafehlandt@gmail.com>
2018-08-16 18:58:29 -0300

    Add overview-card mixin

--- src/app/overview/overview-card/overview-card.component.ts
+++ src/app/overview/overview-card/overview-card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-overview-card',
   templateUrl: './overview-card.component.html',
-  styleUrls: ['./overview-card.component.css']
+  styleUrls: ['./overview-card.component.scss']
 })
 export class OverviewCardComponent implements OnInit {
 
