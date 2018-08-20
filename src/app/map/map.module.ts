6c47fe9554e9c5c9537ce72675f61ecb9eeac822
Max <mx.olivares.p@gmail.com>
2018-08-20 16:25:24 -0300

    Add providers for the map module

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from './map/map.component';
+import { MapService } from './map.service';
 
 
 @NgModule({
@@ -9,6 +10,9 @@ import { MapComponent } from './map/map.component';
   ],
   declarations: [
     MapComponent,
+  ],
+  providers: [
+    MapService
   ],
   exports: [
     MapComponent,
