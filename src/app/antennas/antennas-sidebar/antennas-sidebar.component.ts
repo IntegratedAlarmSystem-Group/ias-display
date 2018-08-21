e20183f054a4361569256073ad7dfb001fac1452
Jazmine <jamaldonadof@ug.uchile.cl>
2018-08-21 12:34:49 -0300

    Define html and css structure for antennas-sidebar

--- src/app/antennas/antennas-sidebar/antennas-sidebar.component.ts
+++ src/app/antennas/antennas-sidebar/antennas-sidebar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-antennas-sidebar',
   templateUrl: './antennas-sidebar.component.html',
-  styleUrls: ['./antennas-sidebar.component.css']
+  styleUrls: ['./antennas-sidebar.component.scss']
 })
 export class AntennasSidebarComponent implements OnInit {
 
--- src/app/antennas/antennas/antennas.component.ts
+++ src/app/antennas/antennas/antennas.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-antennas',
   templateUrl: './antennas.component.html',
-  styleUrls: ['./antennas.component.css']
+  styleUrls: ['./antennas.component.scss']
 })
 export class AntennasComponent implements OnInit {
 
