15aaa049938a0d32425aebd3abeae2dd5d1fcd47
Jazmine <jamaldonadof@ug.uchile.cl>
2018-08-22 12:48:02 -0300

    Iterate over the groups of antennas to show the information

--- src/app/antennas/antennas-sidebar/antennas-sidebar.component.ts
+++ src/app/antennas/antennas-sidebar/antennas-sidebar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AlarmService } from '../../data/alarm.service';
-import { AntennasService, MapAlarmConfig } from '../antennas.service';
+import { AntennasService, MapAlarmConfig, SidebarAlarmConfig } from '../antennas.service';
 import { Alarm } from '../../data/alarm';
 
 @Component({
@@ -39,10 +39,24 @@ export class AntennasSidebarComponent implements OnInit {
    * @param {MapAlarmConfig} antennaConfig the Antenna configuration
    * @returns {string} the antenna name to display
    */
-  getAntennaName(antennaConfig: MapAlarmConfig ): string {
-    const antennaCode = 'DV01';
-    const padCode = antennaConfig.placemark;
-    return  antennaCode + ' (' + padCode + ')';
+  getAntennaName(antennaConfig: SidebarAlarmConfig ): string {
+    return  antennaConfig.antenna + ' (' + antennaConfig.placemark + ')';
+  }
+
+  /**
+  * Return the list of antennas groups Ids defined in the configuration
+  * @returns {list} List of groups IDS
+  */
+  getAntennasGroups(): string [] {
+    return Object.keys(this.antennasService.sidebarAlarmsConfig);
+  }
+
+  /**
+  * Return the list of Alarm configuration by group
+  * @returns {list} list of {@link SidebarAlarmConfig}
+  */
+  getAntennasByGroup(groupID: string): SidebarAlarmConfig [] {
+    return this.antennasService.sidebarAlarmsConfig[groupID];
   }
 
 }
