4e3be134bdc044bed842be2590949ac4b0974609
Sebastian <sebafehlandt@gmail.com>
2018-08-31 13:52:46 -0300

    Test redirection when clicking on summaries. Closes #65

--- src/app/weather/weather-summary/weather-summary.component.spec.ts
+++ src/app/weather/weather-summary/weather-summary.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 import { IasMaterialModule } from '../../ias-material/ias-material.module';
 import { DataModule } from '../../data/data.module';
@@ -10,8 +12,9 @@ import { AlarmService } from '../../data/alarm.service';
 import { WeatherService } from '../weather.service';
 
 
-fdescribe('WeatherSummaryComponent', () => {
+describe('WeatherSummaryComponent', () => {
   let component: WeatherSummaryComponent;
+  let debug: DebugElement;
   let fixture: ComponentFixture<WeatherSummaryComponent>;
   let alarmService: AlarmService;
   const spyRoutingTable = jasmine.createSpyObj('RoutingService', ['tableWithFilter', 'goToWeather']);
@@ -43,6 +46,7 @@ fdescribe('WeatherSummaryComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WeatherSummaryComponent);
     component = fixture.componentInstance;
+    debug = fixture.debugElement;
     alarmService = fixture.debugElement.injector.get(AlarmService);
     fixture.detectChanges();
   });
@@ -53,17 +57,32 @@ fdescribe('WeatherSummaryComponent', () => {
 
   describe('WHEN the component calls goToTableFilteredBy', () => {
     it('THEN the RoutingService.tableWithFilter is called', () => {
+      const old_call_count = spyRoutingTable.tableWithFilter.calls.count();
       component.goToTableFilteredBy('mock filter');
-      expect(spyRoutingTable.tableWithFilter.calls.count()).toBe(1, 'spy method was called once');
+      const new_call_count = spyRoutingTable.tableWithFilter.calls.count();
+      expect(new_call_count).toBe(old_call_count + 1, 'tableWithFilter() was called once');
       expect(spyRoutingTable.tableWithFilter.calls.mostRecent().args[0]).
-        toBe('mock filter', 'spy method was called with the right parameters');
+        toBe('mock filter', 'tableWithFilter was called with the right parameters');
     });
   });
 
   describe('WHEN the component calls redirect', () => {
     it('THEN the RoutingService.goToWeather is called', () => {
+      const old_call_count = spyRoutingTable.goToWeather.calls.count();
       component.redirect();
-      expect(spyRoutingTable.goToWeather.calls.count()).toBe(1, 'spy method was called once');
+      const new_call_count = spyRoutingTable.goToWeather.calls.count();
+      expect(new_call_count).toBe(old_call_count + 1, 'goToWeather() was called once');
+    });
+  });
+
+  describe('AND WHEN the user clicks on it', () => {
+    it('THEN the RoutingService.goToWeather is called', () => {
+      const old_call_count = spyRoutingTable.goToWeather.calls.count();
+      const mainPanel = debug.query(By.css('.content')).nativeElement;
+      mainPanel.click();
+      const new_call_count = spyRoutingTable.goToWeather.calls.count();
+      expect(mainPanel).toBeTruthy();
+      expect(new_call_count).toBe(old_call_count + 1, 'goToWeather() was called once');
     });
   });
 });
