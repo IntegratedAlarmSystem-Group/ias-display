188cfc9965b8a8d8bff53543e26a156a5be7c2ca
Sebastian <sebafehlandt@gmail.com>
2018-10-11 20:46:16 -0300

    Test antennas markers combinations

--- src/app/antennas/antennas-map-markers/antenna-marker/antenna-marker.component.spec.ts
+++ src/app/antennas/antennas-map-markers/antenna-marker/antenna-marker.component.spec.ts
@@ -2,15 +2,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AntennaMarkerComponent } from './antenna-marker.component';
 import { DataModule } from '../../../data/data.module';
 import { AntennasService } from '../../../antennas/antennas.service';
-import { Alarm } from '../../../data/alarm';
+import { Alarm, OperationalMode } from '../../../data/alarm';
 
 describe('AntennaMarkerComponent', () => {
   let component: AntennaMarkerComponent;
   let fixture: ComponentFixture<AntennaMarkerComponent>;
   const mockAlarm = {
-    'value': 4,
-    'core_id': 'critical',
-    'running_id': 'critical',
+    'value': 0,
+    'core_id': 'alarmID',
+    'running_id': 'alarmID',
     'mode': 5,
     'core_timestamp': 1267252440000,
     'state_change_timestamp': 1267252440000,
@@ -40,5 +40,50 @@ describe('AntennaMarkerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('getNgClass should return appropriate values according to Alarm states', () => {
+    expect(component.getNgClass()).toEqual(['green']);
+    component.alarm.value = 1;
+    expect(component.getNgClass()).toEqual(['yellow']);
+    component.alarm.shelve();
+    expect(component.getNgClass()).toEqual(['green']);
+    component.alarm.unshelve();
+    expect(component.getNgClass()).toEqual(['yellow']);
+    component.alarm.value = 2;
+    expect(component.getNgClass()).toEqual(['yellow']);
+    component.alarm.value = 3;
+    expect(component.getNgClass()).toEqual(['red']);
+    component.alarm.value = 4;
+    expect(component.getNgClass()).toEqual(['red']);
+    component.alarm.mode = OperationalMode.maintenance;
+    expect(component.getNgClass()).toEqual(['grey']);
+    component.alarm.mode = OperationalMode.shuttedown;
+    expect(component.getNgClass()).toEqual(['grey']);
+    component.alarm.mode = OperationalMode.degraded;
+    expect(component.getNgClass()).toEqual(['red']);
+  });
+
+  it('getNgClass should return appropriate values according to Alarm states for invalid alarms', () => {
+    component.alarm.validity = 0;
+    expect(component.getNgClass()).toEqual(['green', 'unreliable']);
+    component.alarm.value = 1;
+    expect(component.getNgClass()).toEqual(['yellow', 'unreliable']);
+    component.alarm.shelve();
+    expect(component.getNgClass()).toEqual(['green', 'unreliable']);
+    component.alarm.unshelve();
+    expect(component.getNgClass()).toEqual(['yellow', 'unreliable']);
+    component.alarm.value = 2;
+    expect(component.getNgClass()).toEqual(['yellow', 'unreliable']);
+    component.alarm.value = 3;
+    expect(component.getNgClass()).toEqual(['red', 'unreliable']);
+    component.alarm.value = 4;
+    expect(component.getNgClass()).toEqual(['red', 'unreliable']);
+    component.alarm.mode = OperationalMode.maintenance;
+    expect(component.getNgClass()).toEqual(['grey', 'unreliable']);
+    component.alarm.mode = OperationalMode.shuttedown;
+    expect(component.getNgClass()).toEqual(['grey', 'unreliable']);
+    component.alarm.mode = OperationalMode.degraded;
+    expect(component.getNgClass()).toEqual(['red', 'unreliable']);
   });
 });
