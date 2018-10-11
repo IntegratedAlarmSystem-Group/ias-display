8a93dfca881b8d10ee04aa70896bb73f680cdc74
Sebastian <sebafehlandt@gmail.com>
2018-10-11 20:36:53 -0300

    Fix test

--- src/app/antennas/antennas-map-markers/antenna-marker/antenna-marker.component.spec.ts
+++ src/app/antennas/antennas-map-markers/antenna-marker/antenna-marker.component.spec.ts
@@ -1,13 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AntennaMarkerComponent } from './antenna-marker.component';
-
 import { DataModule } from '../../../data/data.module';
 import { AntennasService } from '../../../antennas/antennas.service';
+import { Alarm } from '../../../data/alarm';
 
 describe('AntennaMarkerComponent', () => {
   let component: AntennaMarkerComponent;
   let fixture: ComponentFixture<AntennaMarkerComponent>;
+  const mockAlarm = {
+    'value': 4,
+    'core_id': 'critical',
+    'running_id': 'critical',
+    'mode': 5,
+    'core_timestamp': 1267252440000,
+    'state_change_timestamp': 1267252440000,
+    'validity': 1,
+    'description': 'my description',
+    'url': 'https://www.alma.cl',
+    'ack': false,
+    'shelved': false,
+    'dependencies': [],
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,6 +34,7 @@ describe('AntennaMarkerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AntennaMarkerComponent);
     component = fixture.componentInstance;
+    component.alarm = Alarm.asAlarm(mockAlarm);
     fixture.detectChanges();
   });
 
