8bd3f6b1ee595763ba0c40ea63a33005e48c93a7
Sebastian <sebafehlandt@gmail.com>
2018-10-11 16:47:13 -0300

    Fix and improve SidenavService tests

--- src/app/actions/sidenav.service.spec.ts
+++ src/app/actions/sidenav.service.spec.ts
@@ -2,8 +2,10 @@ import { TestBed, inject } from '@angular/core/testing';
 import { RoutingService } from '../data/routing.service';
 import { SidenavService } from './sidenav.service';
 
-fdescribe('SidenavService', () => {
-  const spyRoutingService = jasmine.createSpyObj('RoutingService', ['cleanActionOutlet']);
+describe('SidenavService', () => {
+  const spyRoutingService = jasmine.createSpyObj('RoutingService', ['cleanActionOutlet', 'goToShelve', 'goToAcknowledge']);
+  let subject: SidenavService;
+  const mockAlarmId = 'mockAlarmID';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,7 +16,29 @@ fdescribe('SidenavService', () => {
     });
   });
 
-  it('should be created', inject([SidenavService], (service: SidenavService) => {
-    expect(service).toBeTruthy();
+  beforeEach(inject([SidenavService], (service: SidenavService) => {
+    subject = service;
   }));
+
+  it('should be created', () => {
+    expect(subject).toBeTruthy();
+  });
+
+  it('goToShelve() should call routingService.goToShelve() and throw a true in the shouldReload BehaviorSubject', () => {
+    subject.goToShelve(mockAlarmId);
+    expect(spyRoutingService.goToShelve.calls.count()).toBe(1, 'spy method was called once');
+    expect(spyRoutingService.goToShelve.calls.mostRecent().args[0]).
+      toEqual(mockAlarmId, 'spy method was called with the right parameters');
+    subject.shouldReload.subscribe((value) => {
+      expect(value).toEqual(true);
+    });
+  });
+
+  it('goToAcknowledge() should call routingService.goToAcknowledge()', () => {
+    subject.goToAcknowledge(mockAlarmId);
+    expect(spyRoutingService.goToAcknowledge.calls.count()).toBe(1, 'spy method was called once');
+    expect(spyRoutingService.goToAcknowledge.calls.mostRecent().args[0]).
+      toEqual(mockAlarmId, 'spy method was called with the right parameters');
+  });
+
 });
