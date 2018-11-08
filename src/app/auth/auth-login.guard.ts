ce00318aabb5f1417cd238d45393c8876af9bf0e
Sebastian <sebafehlandt@gmail.com>
2018-11-08 16:51:45 -0300

    Make AuthGuard check if AuthLogin has a valid token

--- src/app/auth/auth-login.guard.ts
+++ src/app/auth/auth-login.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -10,8 +11,17 @@ export class AuthLoginGuard implements CanActivate {
 
   constructor(private authService: AuthService) { }
 
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    console.log('Executing canActivate');
-    return true;
+  canActivate() {
+    console.log('Executing AuthLoginGuard.canActivate()');
+    return this.authService.hasValidToken().pipe(map(ok => {
+        if (ok) {
+          return true;
+        } else {
+          // this.router.navigate(['/login']);
+          return false;
+        }
+      }
+    ));
+
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,17 +11,9 @@ export class AuthService {
 
   isLoggedIn = false;
 
-  // store the URL so we can redirect after logging in
-  redirectUrl: string;
-
-  login(): Observable<boolean> {
-    return of(true).pipe(
-      delay(1000),
-      tap(val => this.isLoggedIn = true)
-    );
+  hasValidToken(): Observable<boolean> {
+    console.log('Executing AuthService.hasValidToken()');
+    return of(true);
   }
 
-  logout(): void {
-    this.isLoggedIn = false;
-  }
 }
