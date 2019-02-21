0434238a0b1b04ca02b110fe9815b8ab27de6d0d
Sebastian <sebafehlandt@gmail.com>
2019-02-21 16:52:47 -0300

    Make Table pageOptions an Input and add custom numbers to Table in Antennas

--- src/app/tabular/table/table.component.ts
+++ src/app/tabular/table/table.component.ts
@@ -31,6 +31,11 @@ export class TableComponent implements OnInit, OnDestroy, AfterViewInit {
   */
   @Input() alarmsToDisplay = [];
 
+  /**
+  * Array that defines the options for the number of element to diplay in each page of the table
+  */
+  @Input() pageSizeOptions = [15, 25, 50, 100];
+
   /** Reference to the MatTable, the component that defines the table */
   @ViewChild(MatTable) table: MatTable<Alarm>;
 
