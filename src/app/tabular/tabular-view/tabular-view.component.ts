93258e3e5b11929cd8602305ea1f9edd26446f54
Sebastian <sebafehlandt@gmail.com>
2019-03-08 21:51:12 -0300

    Update some documentation

--- src/app/tabular/tabular-view/tabular-view.component.ts
+++ src/app/tabular/tabular-view/tabular-view.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+* Component that dispays all the Alarms in a table using the {@link TableComponent}
+*/
 @Component({
   selector: 'app-tabular-view',
   templateUrl: './tabular-view.component.html',
@@ -7,8 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabularViewComponent implements OnInit {
 
+  /**
+   * Instantiates the component
+   */
   constructor() { }
 
+  /**
+   * Method executed when the Component is initializated
+   * Does not do anything at the moment
+   */
   ngOnInit() {
   }
 
