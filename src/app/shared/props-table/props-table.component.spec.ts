a1b16518e43274a05af18fa2b7dd90613522f660
Sebastian <sebafehlandt@gmail.com>
2019-03-20 17:07:41 -0300

    Test the PropsTableComponent

--- src/app/shared/props-table/props-table.component.spec.ts
+++ src/app/shared/props-table/props-table.component.spec.ts
@@ -5,6 +5,7 @@ import { Alarm } from '../../data/alarm';
 describe('PropsTableComponent', () => {
   let component: PropsTableComponent;
   let fixture: ComponentFixture<PropsTableComponent>;
+  let tableBody: any;
   const mockAlarm: Alarm = Alarm.asAlarm({
     'value': 4,
     'core_id': 'alarm_1',
@@ -20,7 +21,11 @@ describe('PropsTableComponent', () => {
     'ack': false,
     'shelved': false,
     'dependencies': [],
-    'properties': null,
+    'properties': {
+      'key1': 'value1',
+      'key2': ['value21', 'value22', 'value23'],
+      'key3': 'value3',
+    }
   });
 
   beforeEach(async(() => {
@@ -35,9 +40,28 @@ describe('PropsTableComponent', () => {
     component = fixture.componentInstance;
     component.alarm = mockAlarm;
     fixture.detectChanges();
+    tableBody = fixture.nativeElement.querySelector('table');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Information
+  describe('GIVEN the component has an alarm', () => {
+    beforeEach(() => {
+      component.alarm = mockAlarm;
+      component.ngOnInit();
+      tableBody = fixture.nativeElement.querySelector('table');
+      fixture.detectChanges();
+    });
+
+    it('IT SHOULD display Alarm properties in a Table', () => {
+      expect(tableBody).toBeTruthy();
+      expect(tableBody.textContent.replace(/\s/g, '')).toContain('key1:value1');
+      expect(tableBody.textContent.replace(/\s/g, '')).toContain('key2:value21,value22,value23');
+      expect(tableBody.textContent.replace(/\s/g, '')).toContain('key2:value21,value22,value23');
+      expect(tableBody.textContent.replace(/\s/g, '')).toContain('key3:value3');
+    });
   });
 });
