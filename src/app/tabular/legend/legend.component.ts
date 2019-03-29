b28021c182c9c93d48155ed714e6a767038bf0fa
Maximiliano Olivares <mx.olivares.p@gmail.com>
2019-03-29 11:35:08 -0300

    Update legend priorities

--- src/app/tabular/legend/legend.component.ts
+++ src/app/tabular/legend/legend.component.ts
@@ -7,7 +7,7 @@ import { Assets } from '../../settings';
 @Component({
   selector: 'app-legend',
   templateUrl: './legend.component.html',
-  styleUrls: ['./legend.component.css']
+  styleUrls: ['./legend.component.scss']
 })
 export class LegendComponent implements OnInit {
 
